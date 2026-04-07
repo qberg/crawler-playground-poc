60ba9fc22bd14b51e566bdbf831fe93242a22840
qberg <qbergnoe@gmail.com>
2026-04-07 20:56:41 +0530

    fix

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import { handle } from 'hono/vercel';
 import app from '../server.js';
 
-export const config = { runtime: 'nodejs20.x' };
+export const config = { runtime: 'nodejs' };
 
 export default handle(app);
