13d7a058f43e87fdbaa9c4c81019fe5e35495370
qberg <qbergnoe@gmail.com>
2026-04-07 21:00:04 +0530

    fix made edge

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import { handle } from 'hono/vercel';
 import app from '../server.js';
 
-export const config = { runtime: 'nodejs' };
+export const config = { runtime: 'edge' };
 
 export default handle(app);
